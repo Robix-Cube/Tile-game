6449f8ecc65aff459c675b61e3e9d12bdb70cb1c
Robix Cube <sharp.robertas@gmail.com>
2018-03-17 17:19:02 +0000

    added function for window

--- Init.java
+++ Init.java
@@ -1,3 +1,16 @@
+import javax.swing.*;
+
+
 public class Init
 {
-	
+	public void makeWindow()
+	{
+		JFrame window=new JFrame();
+		window.setVisible(true);
+		window.setTitle("Tile Swap Game");
+		window.setSize(448,360);	
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
+
